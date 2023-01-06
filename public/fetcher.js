79c366db13a93012428ed19ffc6a670783d8a428
Claudio Amilton <pokety@gmail.com>
2023-01-06 18:09:23 -0300

    segundo commit

--- public/fetcher.js
+++ public/fetcher.js
@@ -1,6 +1,13 @@
   const fetcher=async (url)=>{
 
-    let response = await fetch(url);
+    let response = await fetch(url,{
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        headers: {
+        'Content-Type': 'application/json'
+        //'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify(data)
+    });
     let result =await response.json()
     return result
 }
